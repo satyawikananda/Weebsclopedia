590caf0f9c81c7f4e4855af3c0ffad8ce3339c58
Satya Wikananda <satyawikananda456@gmail.com>
2020-03-10 20:39:01 +0800

    :zap: Setting vuetify config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,17 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.cyan.lighten1,
+        secondary: colors.cyan.darken1,
+        accent: colors.grey.lighten2
+      }
+    }
+  }
+});
