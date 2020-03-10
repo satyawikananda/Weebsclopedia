611d254424cb7a6feba68c815120e34557633639
Satya Wikananda <satyawikananda456@gmail.com>
2020-03-10 20:37:31 +0800

    :zap: Setting Dev Server

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  devServer: {
+    host: "weebsclopedia.moe",
+    port: "8080",
+    https: false
+  }
 };
