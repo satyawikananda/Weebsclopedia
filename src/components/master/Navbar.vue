c9028bd0b5c7afce6915bbf104a0c04737f3f581
Satya Wikananda <satyawikananda456@gmail.com>
2020-03-10 20:40:40 +0800

    :sparkles: Adding navbar and home components

---
+++ src/components/master/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://firebasestorage.googleapis.com/v0/b/weebsclopedia.appspot.com/o/img%2F001.png?alt=media&token=51063fad-a321-45bb-9e6b-d7fe2ac43d5f"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <h5 class="title font-weight-regular">Weebsclopedia</h5>
+    </div>
+
+    <v-spacer></v-spacer>
+    <span class="subtitle-1 font-weight-regular mr-5">Anime</span>
+    <span class="subtitle-1 font-weight-regular">Manga</span>
+    <v-spacer></v-spacer>
+
+    <div class="mr-4">
+      <login title="Login" />
+    </div>
+    <div class="mr-4">
+      <v-btn text large color="#00838F">
+        Register
+      </v-btn>
+    </div>
+  </v-app-bar>
+</template>
+<script>
+import login from "../modal/ModalLogin.vue";
+export default {
+  components: {
+    login: login
+  }
+};
+</script>
---
+++ src/views/Root.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="root">
+    hais
+  </div>
+</template>
+<script>
+export default {};
+</script>
