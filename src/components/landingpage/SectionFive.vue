50ba0f2dc911a4ba73a92be26b94b521579d8f35
Satya Wikananda <satyawikananda456@gmail.com>
2020-03-13 20:15:25 +0800

    :sparkles: Adding new feature top ten manga and search manga

---
+++ src/components/landingpage/SectionFive.vue
@@ -0,0 +1,52 @@
+<template>
+  <section id="section3">
+    <v-container>
+      <v-row no-gutters>
+        <v-col cols="12" sm="12" md="12" lg="12">
+          <div class="text-center">
+            <h1 class="text-black display-2 font-weight-light mb-3">
+              Top 10 Manga
+            </h1>
+            <span class="subtitle-1 font-weight-light"
+              >This is the top 10 manga , is there any of your favorite anime
+              included in the ranking?</span
+            >
+            <v-divider />
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" lg="12" md="12" sm="12">
+          <v-sheet class="mx-auto mt-5" elevation="8" max-width="900">
+            <v-slide-group class="pa-4" multiple show-arrows>
+              <v-slide-item v-for="(data, index) in datas" :key="index">
+                <card-top-anime
+                  :title="data.title"
+                  :image="data.image_url"
+                  :rank="data.rank"
+                />
+              </v-slide-item>
+            </v-slide-group>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+<script>
+import CardTopAnime from "@/components/card/CardTopAnime.vue";
+import { dataTopTen } from "@/mixins/getTopTen.js";
+export default {
+  mixins: [dataTopTen],
+  components: {
+    "card-top-anime": CardTopAnime
+  },
+  data() {
+    return {
+      datas: [],
+      subtype: "manga",
+      type: "manga"
+    };
+  }
+};
+</script>
---
+++ src/components/landingpage/SectionFour.vue
@@ -0,0 +1,107 @@
+<template>
+  <section id="sectionFour">
+    <v-row no-gutters>
+      <v-col
+        cols="12"
+        md="12"
+        sm="12"
+        lg="5"
+        id="bg-sectionTwo"
+        :style="{ padding: '50px' }"
+      >
+        <v-container fluid>
+          <v-layout column align-center justify-center>
+            <v-row
+              align="center"
+              justify="space-around"
+              class="d-flex justify-center align-center my-12"
+            >
+              <v-col cols="12" sm="12" md="12" lg="12">
+                <h3
+                  class="display-1 font-weight-reguler text-white text-center"
+                >
+                  Find Your Manga
+                </h3>
+                <v-divider></v-divider>
+              </v-col>
+              <v-col cols="12" sm="12" md="12">
+                <p
+                  class="body-1 font-weight-reguler text-white text-center m-5"
+                >
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis a magni veritatis, illum officia ullam perspiciatis
+                  aliquid repellat eveniet accusantium ut quam beatae, impedit
+                  aliquam doloremque quis tenetur mollitia! Eum.
+                </p>
+              </v-col>
+              <v-col cols="12" sm="8" md="8" class="mx-auto">
+                <v-text-field
+                  label="Solo"
+                  placeholder="Example: Shokugeki no souma"
+                  solo
+                  rounded
+                  v-model="search"
+                  @keyup.enter="getSearch"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-layout>
+        </v-container>
+      </v-col>
+      <v-col
+        cols="12"
+        md="12"
+        sm="12"
+        lg="7"
+        :style="{ padding: '50px', backgroundColor: '#ecf0f1' }"
+      >
+        <v-container fluid>
+          <v-layout column align-center justify-center>
+            <h3 class="display-1 font-weight-reguler text-black text-center">
+              Manga Search: {{ search }}
+            </h3>
+            <v-row no-gutters>
+              <v-col cols="12" sm="12" md="12" lg="12" class="mx-auto mt-5">
+                <v-sheet color="accent">
+                  <v-skeleton-loader
+                    type="card, article"
+                    width="400"
+                    :boilerplate="boilerplate"
+                    v-if="firstLoad"
+                    :loading="loading"
+                  >
+                  </v-skeleton-loader>
+                </v-sheet>
+                <card-anime
+                  v-show="!firstLoad"
+                  :loading="loading"
+                  v-for="(data, index) in datas"
+                  :key="index"
+                  :image="data.image_url"
+                  :title="data.title"
+                  :content="data.synopsis"
+                />
+              </v-col>
+            </v-row>
+          </v-layout>
+        </v-container>
+      </v-col>
+    </v-row>
+  </section>
+</template>
+<script>
+import cardAnime from "@/components/card/CardAnime.vue";
+import { dataCardSearch } from "@/mixins/getFindDataAM.js";
+export default {
+  mixins: [dataCardSearch],
+  data() {
+    return {
+      datas: [],
+      type: "manga"
+    };
+  },
+  components: {
+    "card-anime": cardAnime
+  }
+};
+</script>
